02862f17d6095483bc317d692976b4fa4c9fff04
CorrLLL <TY574800411@163.com>
2024-07-30 13:37:10 +0800

    添加项目文件。

---
+++ OpenCVdemo/OpenCVdemo.cpp
@@ -0,0 +1,11 @@
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace cv;
+
+int main()
+{
+    Mat girl = imread("girl.jpg"); //载入图像到Mat
+    imshow("【1】动漫图", girl);//显示名为 "【1】动漫图"的窗口  
+    waitKey(0);
+    return 0;
